514160b0ad16cbd9ada432fa7d525ff1e4efae1b
Dravya S Rai <83718057+DravyaRai@users.noreply.github.com>
2024-06-04 23:30:07 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns1= [
+urlpatterns= [
     path('', include('members.urls')),
     path('admin/', admin.site.urls),
 ]
